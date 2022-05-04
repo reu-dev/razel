2ae1ffb352359f7e934daec4a99f3e9c0e04ee7f
Stefan Reuschl <stefan.reuschl@gmail.com>
2022-05-04 02:44:40 +0200

    add rule parser

--- src/rules.rs
+++ src/rules.rs
@@ -1,6 +1,20 @@
 use crate::CommandBuilder;
 use std::collections::HashMap;
 
+use anyhow::{bail, Context};
+
+/// Rules to parse input/output file arguments from command lines
+///
+/// Specifying rules:
+/// * first word is the program
+/// * only input (<in>) and output (<out>) file arguments need to be specified, other arguments (<>) will be ignored
+/// * multiple file arguments are marked with ...
+///
+/// Parsing commands using the rules:
+/// * the first command line argument (program) selects the rule(s)
+/// * positional arguments are parsed backwards from the command line
+/// * then named arguments are parsed
+/// * other arguments are ignored
 struct Rules {
     rules: HashMap<String, Rule>,
 }
@@ -17,32 +31,97 @@ impl Rules {
     fn set_defaults(&mut self) {
         vec![
             "cp <in> <out>",
-            "ar <out:.a> <in:.o>",
-            "c++ -MF <out> -o <out> -c <in>",
-            "c++ -o <out> <in:.o> <in:.a>",
-            "cc -MF <out> -o <out> -c <in>",
+            "ar <out> <in>...",
+            "c++ -MF <out> -o <out> <in>...",
+            "cc  -MF <out> -o <out> <in>...",
         ]
         .iter()
-        .map(|x| self.add(x));
+        .for_each(|x| self.add(x));
     }
 }
 
 struct Rule {
     executable: String,
     named_args: HashMap<String, Arg>,
+    positional_args: Vec<Arg>,
 }
 
 impl Rule {
-    pub fn from_string(s: &str) -> Self {}
+    pub fn from_string(spec: &str) -> Result<Self, anyhow::Error> {
+        let mut items = spec.split(' ').filter(|x| !x.is_empty());
+        let executable = items.next().context("Rule is incomplete")?.into();
+        let mut named_args: HashMap<String, Arg> = Default::default();
+        let mut positional_args: Vec<Arg> = Default::default();
+        let mut name: Option<&str> = None;
+        for item in items {
+            let arg = Self::parse_arg(item)?;
+            match (name, arg) {
+                (None, None) => name = Some(item),
+                (Some(n), Some(a)) => {
+                    named_args.insert(n.into(), a);
+                    name = None;
+                }
+                (None, Some(a)) => positional_args.push(a),
+                (Some(n), None) => bail!(format!("named argument without file spec: {n}")),
+            }
+        }
+        if positional_args.iter().filter(|x| x.multiple).count() > 1 {
+            bail!("only one positional argument might take multiple values");
+        }
+        Ok(Self {
+            executable,
+            named_args,
+            positional_args,
+        })
+    }
 
-    pub fn parse(command: &str) -> CommandBuilder {}
+    fn parse_arg(item: &str) -> Result<Option<Arg>, anyhow::Error> {
+        let open = item.chars().into_iter().filter(|x| *x == '<').count();
+        let close = item.chars().into_iter().filter(|x| *x == '>').count();
+        Ok(if open == 0 && close == 0 {
+            None
+        } else if item == "<in>" {
+            Some(Arg {
+                file_type: FileType::Input,
+                multiple: false,
+            })
+        } else if item == "<in>..." {
+            Some(Arg {
+                file_type: FileType::Input,
+                multiple: true,
+            })
+        } else if item == "<out>" {
+            Some(Arg {
+                file_type: FileType::Output,
+                multiple: false,
+            })
+        } else if item == "<out>..." {
+            Some(Arg {
+                file_type: FileType::Output,
+                multiple: true,
+            })
+        } else if item == "<>" {
+            Some(Arg {
+                file_type: FileType::NoFile,
+                multiple: false,
+            })
+        } else {
+            bail!(format!("syntax error: {item}"))
+        })
+    }
+
+    pub fn parse(command_line: &str) -> CommandBuilder {
+        todo!()
+    }
 }
 
 struct Arg {
     file_type: FileType,
+    multiple: bool,
 }
 
 enum FileType {
     Input,
     Output,
+    NoFile,
 }
