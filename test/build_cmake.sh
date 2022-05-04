c39e5c8451e481def3aeeed00f3121619ffe28b4
Stefan Reuschl <stefan.reuschl@gmail.com>
2022-05-04 03:00:02 +0200

    cleanup build_cmake.sh

--- test/build_cmake.sh
+++ test/build_cmake.sh
@@ -11,19 +11,19 @@ if [ ! -d $ROOT_DIR ]; then
   git clone https://gitlab.kitware.com/cmake/cmake.git -b release --single-branch $CMAKE_SOURCE_DIR
 fi
 
-# configure simple version of cmake
 rm -rf $CMAKE_BINARY_DIR
 
-#mkdir -p $CMAKE_BINARY_DIR/.cmake/api/v1/query
-#touch $CMAKE_BINARY_DIR/.cmake/api/v1/query/codemodel-v2
-#touch $CMAKE_BINARY_DIR/.cmake/api/v1/query/cache-v2
-#touch $CMAKE_BINARY_DIR/.cmake/api/v1/query/cmakeFiles-v1
-#touch $CMAKE_BINARY_DIR/.cmake/api/v1/query/toolchains-v1
+# configure simple version of cmake
+# patch CMAKE_C_ARCHIVE_* to simplify linker script (get rid of ranlib)
+cmake -S $CMAKE_SOURCE_DIR -B $CMAKE_BINARY_DIR -D BUILD_CursesDialog=OFF -D BUILD_QtDialog=OFF -D BUILD_TESTING=OFF -D CMAKE_BUILD_TYPE=Debug \
+  -D "CMAKE_C_ARCHIVE_CREATE=<CMAKE_AR> qcs <TARGET> <LINK_FLAGS> <OBJECTS>" -D "CMAKE_C_ARCHIVE_FINISH=" \
+  -G Ninja
 
-# TODO simplify linker script?
-# CMAKE_C_ARCHIVE_CREATE "<CMAKE_AR> qcs <TARGET> <LINK_FLAGS> <OBJECTS>"
-# CMAKE_C_ARCHIVE_FINISH ""
-
-cmake -S $CMAKE_SOURCE_DIR -B $CMAKE_BINARY_DIR -D BUILD_CursesDialog=OFF -D BUILD_QtDialog=OFF -D BUILD_TESTING=OFF -D CMAKE_BUILD_TYPE=Debug -G Ninja
-
+# build commands used by ninja with razel
 ninja -C $CMAKE_BINARY_DIR -t commands > $CMAKE_BINARY_DIR/commands.sh
+cargo run -- batch $CMAKE_BINARY_DIR/commands.sh
+
+# build with ninja to get a reference executable
+ninja -C $CMAKE_BINARY_DIR
+
+diff $CMAKE_BINARY_DIR/bin/cmake razel-bin/bin/cmake
