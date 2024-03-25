5be708d89dca00cbbf999eb5b422e3df7e33d49d
reu-dev <reu-dev@users.noreply.github.com>
2024-03-25 23:46:38 +0000

    Release v0.4.0

--- apis/python/razel.py
+++ apis/python/razel.py
@@ -13,7 +13,7 @@ from collections.abc import Mapping, Sequence
 
 
 class Razel:
-    version: ClassVar[str] = "0.3.1"
+    version: ClassVar[str] = "0.4.0"
     _instance: ClassVar[Optional[Razel]] = None
 
     class Tag(str, Enum):
