dae76f7715ee22ec56ca8d1f7042c75b5db16f03
Stefan Reuschl <stefan@reu-dev.de>
2024-11-04 21:52:39 +0100

    more verbose self-test

--- razel-self-test/src/main.rs
+++ razel-self-test/src/main.rs
@@ -19,11 +19,19 @@ struct Args {
 fn main() -> ExitCode {
     let args = Args::parse();
 
-    let mut _vec: Option<Vec<u8>> = args.memory.map(|x| vec![37; x]);
+    let mut vec: Vec<u8> = vec![];
+    if let Some(x) = args.memory {
+        println!("allocate {x} bytes...");
+        vec.resize(x, x as u8);
+    }
 
     if let Some(x) = args.sleep {
+        println!("sleep for {x}s...");
         sleep(Duration::from_secs_f32(x));
     }
 
+    if args.exit_code != 0 {
+        eprintln!("exit with code {}", args.exit_code);
+    }
     ExitCode::from(args.exit_code)
 }
